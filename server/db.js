08759e181788100f9d176a03a80c780ce41143cd
shenqjye93 <shenqjye93@gmail.com>
2025-06-13 15:10:57 +0800

    prepare app for deployment

--- server/db.js
+++ server/db.js
@@ -2,12 +2,20 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 // The Pool will manage multiple client connections for us.
+// const pool = new Pool({
+// 	user: process.env.DB_USER,
+// 	host: process.env.DB_HOST,
+// 	database: process.env.DB_DATABASE,
+// 	password: process.env.DB_PASSWORD,
+// 	port: process.env.DB_PORT,
+// });
+const connectionString = process.env.DB_CONNECTION_STRING;
+
 const pool = new Pool({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.DB_DATABASE,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT,
+	connectionString: connectionString,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
 // We export a single query function to use throughout our app.
