8dc76c51f27b4d0d93ca8a03020ddfc43c833ba8
shenqjye93 <shenqjye93@gmail.com>
2025-06-13 16:12:11 +0800

    update api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 const API_KEY = "SECRET12345"; // Use the same key as in the backend .env
 
 const apiClient = axios.create({
